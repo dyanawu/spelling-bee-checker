af35678742ba155227dc1e47067838672f018a64
Dyana Wu <dyanawu@gmail.com>
2022-04-25 18:06:51 +0800

    Update output format

--- spelling-bee-checker.py
+++ spelling-bee-checker.py
@@ -10,16 +10,11 @@ infile = open(filename, "r")
 words = infile.read().splitlines()
 infile.close()
 
-two_letter_list = {}
+word_lengths = []
 letter_count = {}
+two_letter_list = {}
 
 for word in words:
-    tl = word[:2]
-    if tl in two_letter_list:
-        two_letter_list[tl] += 1
-    else:
-        two_letter_list[tl] = 1
-
     length = len(word)
     sl = word[:1]
     if sl not in letter_count:
@@ -30,5 +25,33 @@ for word in words:
     else:
         letter_count[sl][length] = 1
 
-pprint.pprint(letter_count)
-pprint.pprint(two_letter_list)
+    tl = word[:2]
+    if sl not in two_letter_list:
+        two_letter_list[sl] = {}
+
+    if tl in two_letter_list[sl]:
+        two_letter_list[sl][tl] += 1
+    else:
+        two_letter_list[sl][tl] = 1
+
+    if length not in word_lengths:
+        word_lengths.append(length)
+    else:
+        continue
+
+word_lengths.sort()
+letter_count_grid = {}
+
+for sl in letter_count:
+    for i in range (word_lengths[0], word_lengths[-1]+1):
+        letter_count_grid[sl] = [str(letter_count[sl][i]) if i in letter_count[sl] else "-" for i in word_lengths]
+
+print("\nWord lengths:\n")
+print(f"    {'  '.join([str(i) for i in word_lengths])}")
+for row in letter_count_grid:
+    print(f"{row}:  " + "  ".join(letter_count_grid[row]))
+
+print("\nTwo letter list:\n")
+for tl in two_letter_list:
+    tl_list = [f"{key.upper()}-{value}" for key, value in two_letter_list[tl].items()]
+    print(" ".join(tl_list))
